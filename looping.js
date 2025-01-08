f24116eab870a160a0c65d1cb223c22f3789b0c3
vinnie0807 <vinaykumarb518@gmail.com>
2025-01-08 11:32:08 +0530

    Update files

--- looping.js
+++ looping.js
@@ -1,3 +1,26 @@
-for(let i=1;i<=5;i++){
-    console.log("hello world");
-}
+// for(let i=1;i<=5;i++){
+// //     console.log("hello world");
+//    if(i%2 ==0){
+//     console.log(i +"is even number");
+//    }else{
+//     console.log(i+"is odd number")
+//    }
+// }
+// num=5
+// let fact = 1;
+// for(let i=1;i<=num;i++){
+//     fact = fact*i
+// }
+// console.log(fact);
+
+// let i = 1;
+// while(i<5){
+//     console.log("GN");
+//     i++;
+
+// }
+let i =1;
+do{
+    console.log("Please wake up");
+    i++;
+}while(i<=5);
