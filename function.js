3682776a0f499040c78b42391cb6bcc104ec564d
vinnie0807 <vinaykumarb518@gmail.com>
2025-01-08 11:52:39 +0530

    new files add

---
+++ function.js
@@ -0,0 +1,23 @@
+// without parameter and argumements
+// function demo(){
+//     console.log("Hello i am function demo")
+// }
+// demo();
+
+// with parameter and argumements
+function add(a,b){
+    // console.log(a);
+    // console.log(b);
+    console.log(a+b);
+}
+add(5,6)
+
+// function add2(a,b){
+//     // console.log(a);
+//     // console.log(b);
+//     console.log(a+b);
+// }
+// add2(7,58)
+function addtocart(){
+    document.body.style.backgroundColor = "red";
+}
--- looping.js
+++ looping.js
@@ -1,26 +1,26 @@
-// for(let i=1;i<=5;i++){
-// //     console.log("hello world");
-//    if(i%2 ==0){
-//     console.log(i +"is even number");
-//    }else{
-//     console.log(i+"is odd number")
-//    }
-// }
-// num=5
-// let fact = 1;
-// for(let i=1;i<=num;i++){
-//     fact = fact*i
-// }
-// console.log(fact);
+// // for(let i=1;i<=5;i++){
+// // //     console.log("hello world");
+// //    if(i%2 ==0){
+// //     console.log(i +"is even number");
+// //    }else{
+// //     console.log(i+"is odd number")
+// //    }
+// // }
+// // num=5
+// // let fact = 1;
+// // for(let i=1;i<=num;i++){
+// //     fact = fact*i
+// // }
+// // console.log(fact);
 
-// let i = 1;
-// while(i<5){
-//     console.log("GN");
+// // let i = 1;
+// // while(i<5){
+// //     console.log("GN");
+// //     i++;
+
+// // }
+// let i =1;
+// do{
+//     console.log("Please wake up");
 //     i++;
-
-// }
-let i =1;
-do{
-    console.log("Please wake up");
-    i++;
-}while(i<=5);
+// }while(i<=5);
