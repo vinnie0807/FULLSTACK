e45351fe9ee5e6c17f1cab4908f388c8715d4c73
vinnie0807 <vinaykumarb518@gmail.com>
2025-01-23 10:28:23 +0530

    files are updated

--- api.js
+++ api.js
@@ -1,22 +1,43 @@
-function fetchUsers(){
-    let res = window.fetch("https://jsonplaceholder.typicode.com/users");
-    // console.log(res);
-    res.then((response)=>{
-        return response.json().then(data=>{
-            console.log(data);
-            let store = document.getElementById("store");
-            data.map((user)=>{
-                store.innerHTML +=`
-                 <tr>
-                    <td>${user.id}</td>
-                    <td>${user.name}</td> 
-                    <td>${user.email}</td>
-                    <td>${user.company.name}</td>
-                </tr>` 
+// function fetchUsers(){
+//     let res = window.fetch("https://jsonplaceholder.typicode.com/users");
+//     // console.log(res);
+//     res.then((response)=>{
+//         return response.json().then(data=>{
+//             console.log(data);
+//             let store = document.getElementById("store");
+//             data.map((user)=>{
+//                 store.innerHTML +=`
+//                  <tr>
+//                     <td>${user.id}</td>
+//                     <td>${user.name}</td> 
+//                     <td>${user.email}</td>
+//                     <td>${user.company.name}</td>
+//                 </tr>` 
                
-            })
-        })
-    })
-    .catch(err=>console.log(err))
+//             })
+//         })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchUsers();
+
+// let p = new Promise((resolve, reject) => { 
+//     setTimeout(()=>{
+//         resolve("success");
+//     },3000);
+// });
+
+// async function demo(){
+//     console.log("start");
+//     let x = await p;
+//     console.log(x);
+//     console.log("end");
+// }
+// demo();
+
+async function fetchUsers(){
+    let res = await fetch("https://jsonplaceholder.typicode.com/users"); 
+    let data = await res.json();
+    console.log(data);
 }
 fetchUsers();
---
+++ dom.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("demo");
+element.innerHTML = "DOM";
+console.log(element);
+
+let div =  document.getElementById("test");
+div.innerHTML = "<h2>header</h2>";
+console.log(div);
