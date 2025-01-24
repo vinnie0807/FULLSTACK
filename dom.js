25ac2e51fca66e022dfc53bf7bbe97e53180e27c
vinnie0807 <vinaykumarb518@gmail.com>
2025-01-24 10:29:31 +0530

    files are updated

--- dom.js
+++ dom.js
@@ -1,7 +1,57 @@
-let element = document.getElementById("demo");
-element.innerText = "DOM";
-console.log(element);
+// // let element = document.getElementById("demo");
+// // element.innerText = "DOM";
+// // console.log(element);
 
-let div =  document.getElementById("test");
-div.innerHTML = "<h2>header</h2>";
-console.log(div);
+// // let div =  document.getElementById("test");
+// // div.innerHTML = "<h2>header</h2>";
+// // console.log(div);
+
+// let ele =  document.getElementsByClassName("test");
+// console.log(Array.isArray(ele))
+// ele[0].style.backgroundColor = "red";
+// ele[1].style.backgroundColor = "green";
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// x.map((element)=>{
+//     element.style.backgroundColor = "teal";
+// });
+
+// let container=document.getElementsByClassName("container");
+// console.log(Array.isArray(container)) //false {not pure array}
+// //using spread operator
+// container[0].style.backgroundColor="yellow"
+// let x=[...container]
+// console.log(Array.isArray(x)) //true {pure array}
+
+// // Now we can use map method for iterating
+// x.map(element=>{console.log(element)
+//     element.style.backgroundColor="aqua"
+// });
+
+// let ele = document.querySelector("demo");
+// console.log(ele);
+//null output
+
+// let ele = document.querySelector(".container");
+// console.log(ele);
+
+
+// let ele = document.querySelectorAll(".container");
+// [...ele].map((element)=>{
+//     console.log(element.innerText);
+// });
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", function(){
+//     console.log("Button clicked");
+// });
+// let btn2 = document.getElementById("btn");
+// btn2.addEventListener("mouseover", function(){
+//     console.log("Button clicked");
+//     document.body.style.backgroundColor = "red";
+// });
+
+let btn = document.getElementById("btn");
+btn.addEventListener("dblclick", function(){
+    console.log("Button clicked");
+    document.body.style.backgroundColor = "red";
+});
